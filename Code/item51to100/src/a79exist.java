a59ca69f0d6f3e7c888f92fbf161a78d3a290022
SlowHot <244167780@qq.com>
2021-06-07 21:40:26 +0800

    6.7

--- Code/item51to100/src/a79exist.java
+++ Code/item51to100/src/a79exist.java
@@ -1,11 +1,21 @@
 public class a79exist {
     public static void main(String[] args) {
+//        char[][] board = {
+//                {'A', 'B', 'C', 'E'},
+//                {'S', 'F', 'C', 'S'},
+//                {'A', 'D', 'E', 'E'}
+//        };
+//        String word = "ABCCED";
+//        char[][] board = {
+//                {'A', 'B', 'C', 'E'},
+//                {'S', 'F', 'C', 'S'},
+//                {'A', 'D', 'E', 'E'}
+//        };
+//        String word = "ABCB";
         char[][] board = {
-                {'A', 'B', 'C', 'E'},
-                {'S', 'F', 'C', 'S'},
-                {'A', 'D', 'E', 'E'}
+                {'A', 'B'}
         };
-        String word = "ABCCED";
+        String word = "BA";
         System.out.println(exist(board, word));
     }
 
@@ -15,8 +25,7 @@ public class a79exist {
         int[][] visited = new int[row][col];
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                boolean flag = DFS(board, visited, word, i, j, 0);
-                if (flag) {
+                if (board[i][j] == word.charAt(0) && DFS(board, visited, word, i, j, 0)) {
                     return true;
                 }
             }
@@ -26,25 +35,26 @@ public class a79exist {
 
     //DFS
     public static boolean DFS(char[][] board, int[][] visited, String word, int i, int j, int k) {
-        if (k == word.length()) {
+        visited[i][j] = 1;
+        if (board[i][j] != word.charAt(k)) {
+            visited[i][j] = 0;
+            return false;
+        } else if (k == word.length() - 1) {
             return true;
         }
-        if (i - 1 >= 0 && visited[i - 1][j] == 0 && board[i - 1][j] == word.charAt(k)) {
-            visited[i - 1][j] = 1;
-            DFS(board, visited, word, i - 1, j, k + 1);
+        if (i - 1 >= 0 && visited[i - 1][j] == 0 && DFS(board, visited, word, i - 1, j, k + 1)) {
+            return true;
         }
-        if (i + 1 < board.length && visited[i + 1][j] == 0 && board[i + 1][j] == word.charAt(k)) {
-            visited[i + 1][j] = 1;
-            DFS(board, visited, word, i + 1, j, k + 1);
+        if (i + 1 < board.length && visited[i + 1][j] == 0 && DFS(board, visited, word, i + 1, j, k + 1)) {
+            return true;
         }
-        if (j - 1 >= 0 && visited[i][j - 1] == 0 && board[i][j - 1] == word.charAt(k)) {
-            visited[i][j - 1] = 1;
-            DFS(board, visited, word, i, j - 1, k + 1);
+        if (j - 1 >= 0 && visited[i][j - 1] == 0 && DFS(board, visited, word, i, j - 1, k + 1)) {
+            return true;
         }
-        if (j + 1 >= 0 && visited[i][j + 1] == 0 && board[i][j + 1] == word.charAt(k)) {
-            visited[i][j + 1] = 1;
-            DFS(board, visited, word, i, j + 1, k + 1);
+        if (j + 1 < board[0].length && visited[i][j + 1] == 0 && DFS(board, visited, word, i, j + 1, k + 1)) {
+            return true;
         }
+        visited[i][j] = 0;
         return false;
     }
 }
