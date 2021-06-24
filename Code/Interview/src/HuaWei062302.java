036ddb09883ba02b54e4c4c9408932189c162e7f
SlowHot <244167780@qq.com>
2021-06-24 19:36:54 +0800

    6.24 Huawei02

--- Code/Interview/src/HuaWei062302.java
+++ Code/Interview/src/HuaWei062302.java
@@ -5,74 +5,68 @@ import java.util.Scanner;
 public class HuaWei062302 {
 
     public static int freshTomato = 0;
-    public static int day = 0;
-
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int M = Integer.parseInt(scanner.next());
         int N = Integer.parseInt(scanner.next());
         char[][] tomatoGrid = new char[M][N];
+        char[][] tomatoGridTemp = new char[M][N];
+        int[][] dayTomatoGrid = new int[M][N];
+
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
                 tomatoGrid[i][j] = Character.toUpperCase(scanner.next().charAt(0));
+                tomatoGridTemp[i][j] = tomatoGrid[i][j];
+                dayTomatoGrid[i][j] = Integer.MAX_VALUE;
             }
         }
-        int max = 0;
+
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
-                if (tomatoGrid[i][j] =='B'){
-                    DFS(tomatoGrid,i,j);
+                if (tomatoGrid[i][j] == 'B') {
+                    DFS(tomatoGrid, dayTomatoGrid, i, j, 0);
+//                    for (int k = 0; k < tomatoGrid.length; k++) {
+//                        tomatoGrid[k] = tomatoGridTemp[k].clone();
+//                    }
                 }
             }
         }
-
-    }
-    public static void BFS(char[][] tomatoGrid, int i, int j){
-        if (i >= tomatoGrid.length || j >= tomatoGrid[0].length || i < 0 || j < 0) {
-            return;
-        }
-        if (tomatoGrid[i][j] == 'B'){
-            ArrayDeque<Integer> arrayDeque = new ArrayDeque<Integer>();
-        }
-
     }
 
-
-    public static void DFS(char[][] tomatoGrid, int i, int j) {
+    public static void DFS(char[][] tomatoGrid, int[][] dayTomatoGrid, int i, int j, int day) {
         if (i >= tomatoGrid.length || j >= tomatoGrid[0].length || i < 0 || j < 0) {
             return;
         }
         if (tomatoGrid[i][j] == 'E') {
             return;
         }
-        if (tomatoGrid[i][j] == 'B') {
-            boolean flag = false;
-            if (i + 1 < tomatoGrid.length && tomatoGrid[i + 1][j] == 'R') {
-                flag = true;
-                tomatoGrid[i + 1][j] = 'B';
-                DFS(tomatoGrid, i + 1, j);
 
-            }
-            if (i - 1 >= 0 && tomatoGrid[i - 1][j] == 'R') {
-                flag = true;
-                tomatoGrid[i - 1][j] = 'B';
-                DFS(tomatoGrid, i - 1, j);
-            }
-            if (j + 1 < tomatoGrid[0].length && tomatoGrid[i][j + 1] == 'R') {
-                flag = true;
-                tomatoGrid[i][j + 1] = 'B';
-                DFS(tomatoGrid, i, j + 1);
-            }
-            if (j - 1 >= 0 && tomatoGrid[i][j - 1] == 'R') {
-                flag = true;
-                tomatoGrid[i][j - 1] = 'B';
-                DFS(tomatoGrid, i, j - 1);
-            }
-            if (flag){
-                day++;
-            }
+        if (i + 1 < tomatoGrid.length && tomatoGrid[i + 1][j] == 'R') {
+            tomatoGrid[i + 1][j] = 'B';
+            dayTomatoGrid[i + 1][j] = Math.min(day + 1, dayTomatoGrid[i + 1][j]);
+            DFS(tomatoGrid, dayTomatoGrid, i + 1, j, day + 1);
+            tomatoGrid[i + 1][j] = 'R';
         }
+        if (i - 1 >= 0 && tomatoGrid[i - 1][j] == 'R') {
+            tomatoGrid[i - 1][j] = 'B';
+            dayTomatoGrid[i - 1][j] = Math.min(day + 1, dayTomatoGrid[i - 1][j]);
+            DFS(tomatoGrid, dayTomatoGrid, i - 1, j, day + 1);
+            tomatoGrid[i - 1][j] = 'R';
+        }
+        if (j + 1 < tomatoGrid[0].length && tomatoGrid[i][j + 1] == 'R') {
+            tomatoGrid[i][j + 1] = 'B';
+            dayTomatoGrid[i][j + 1] = Math.min(day + 1, dayTomatoGrid[i][j + 1]);
+            DFS(tomatoGrid, dayTomatoGrid, i, j + 1, day + 1);
+            tomatoGrid[i][j + 1] = 'R';
+        }
+        if (j - 1 >= 0 && tomatoGrid[i][j - 1] == 'R') {
+            tomatoGrid[i][j - 1] = 'B';
+            dayTomatoGrid[i][j - 1] = Math.min(day + 1, dayTomatoGrid[i][j - 1]);
+            DFS(tomatoGrid, dayTomatoGrid, i, j - 1, day + 1);
+            tomatoGrid[i][j - 1] = 'R';
+        }
+
         return;
     }
 
