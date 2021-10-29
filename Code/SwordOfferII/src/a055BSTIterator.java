1d0e8c5f91cbefae2114a61fd6abd73c26425cd3
Lab <244167780@qq.com>
2021-10-29 13:33:16 +0800

    setting

---
+++ Code/SwordOfferII/src/a055BSTIterator.java
@@ -0,0 +1,19 @@
+public class a055BSTIterator {
+    public static void main(String[] args) {
+
+    }
+}
+class BSTIterator {
+
+    public BSTIterator(TreeNode root) {
+
+    }
+
+    public int next() {
+
+    }
+
+    public boolean hasNext() {
+
+    }
+}
