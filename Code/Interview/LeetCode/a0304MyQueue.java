95075738fb55e15ce17fd4bf9421a2b90070ef94
Lab <244167780@qq.com>
2021-12-06 12:50:30 +0800

    12.06

--- Code/Interview/LeetCode/a0304MyQueue.java
+++ Code/Interview/LeetCode/a0304MyQueue.java
@@ -4,41 +4,4 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class a0304MyQueue {
 
-    static class MyQueue {
-        /**
-         * Initialize your data structure here.
-         */
-        public MyQueue() {
-
-        }
-
-        /**
-         * Push element x to the back of queue.
-         */
-        public void push(int x) {
-
-        }
-
-        /**
-         * Removes the element from in front of queue and returns that element.
-         */
-        public int pop() {
-
-        }
-
-        /**
-         * Get the front element.
-         */
-        public int peek() {
-
-        }
-
-        /**
-         * Returns whether the queue is empty.
-         */
-        public boolean empty() {
-
-        }
-    }
-    
 }
