43fa53c3e1022b6cda8e5dd5c33db94a70db411f
SlowHotLAPTOP <244167780@qq.com>
2022-04-03 18:38:33 +0800

    irl

---
+++ Code/Interview/JUC/sync.java
@@ -0,0 +1,44 @@
+public class sync {
+    public static void main(String[] args) {
+        /*创建多线程的一般流程*/
+        // 1. 创建资源类,在资源类中创建属性和方法 (为了实现高内聚,低耦合)
+        Ticket ticket = new Ticket();
+        // 2. 创建多线程,调用资源类方法
+
+        new Thread(() -> {
+            for (int i = 0; i < 40; i++) {
+                ticket.sale();
+            }
+        }, "seller 1").start();
+        new Thread(() -> {
+            for (int i = 0; i < 40; i++) {
+                ticket.sale();
+            }
+        }, "seller 2").start();
+        new Thread(() -> {
+            for (int i = 0; i < 40; i++) {
+                ticket.sale();
+            }
+        }, "seller 3").start();
+
+
+    }
+
+    /*一个多线程例子*/
+    /*3个售票员 售出30张票*/
+    static class Ticket {
+        private int number = 30;
+
+        private synchronized void sale() {
+            System.out.println(Thread.currentThread().getName());
+            if (number <= 0) {
+                System.out.println("Ticket out...");
+                return;
+            }
+            System.out.println("sale 1 ticket,remain " + number--);
+        }
+
+    }
+}
+
+
